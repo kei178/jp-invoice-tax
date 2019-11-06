a55001bc59c65beb15a4760d64dce8befbd5d446
Kei178 <keisuke.inaba.cpa@gmail.com>
2019-11-06 21:38:55 +1100

    Add hiding instruction

--- src/components/Instruction.vue
+++ src/components/Instruction.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="ui segment basic">
     <h1>課税事業者成りシミュレーター</h1>
-    <div class="ui divider hidden"></div>
-    <div class="ui segment">
-      <h3>当シミュレーターの説明</h3>
+    <p 
+      class="header" 
+      @click="showInstruction = !showInstruction">説明文をみる</p>
+    <div class="ui segment" v-if="showInstruction">
       <p>
         2023年10月から始まるインボイス制度。もし何も対策をしなければ、これのせいで<strong>フリーランスの収入は10%も減る可能性があります。</strong>
         一方、事前に対策をおこない（＝課税事業者になって）、引き続き消費税を請求できるようにすれば、<strong>年収減は半分以下に抑えられます。</strong>
       </p>
       <p>
-        このシミュレーターは、設定値をもとに「課税事業者成り」で見込める手取りの増加額をざっくり計算します。
+        このシミュレーターでは、設定値を入力するだけで「課税事業者成り」で見込める手取りの増加額をざっくり計算します。
         少しでも年収減を食い止められるように、2022年までにしっかり準備しておきましょう！
       </p>
       <p>
@@ -22,7 +23,9 @@
 <script>  
 export default {
   data() {
-    return {}
+    return {
+      showInstruction: false
+    }
   }
 }
 </script>
@@ -30,5 +33,10 @@ export default {
 <style scoped>
   h1 {
     text-align: center;
+  }
+  p.header {
+     text-align: center;
+     cursor: pointer;
+     color: #2185d0;
   }
 </style>
