f982ec7c38f53db57f7bfe6a155ac20b2616546e
Kei178 <keisuke.inaba.cpa@gmail.com>
2019-11-08 11:17:38 +1100

    Add new components

---
+++ src/components/Contact.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="ui segment basic center aligned">
+    <h2>開発者をフォローする</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+</style>    
