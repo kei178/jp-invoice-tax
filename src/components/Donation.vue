207975658e298ef4d23b0f5073091b0ecfec6fc5
Kei178 <keisuke.inaba.cpa@gmail.com>
2019-11-09 12:09:17 +1100

    Added icons

--- src/components/Donation.vue
+++ src/components/Donation.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="ui segment basic center aligned">
-    <h2>このページを応援する</h2>
+    <h2>
+      このページを応援
+      <i class="thumbs up outline icon"></i>
+    </h2>
   </div>
 </template>
 
