470104e8cd6a0427e943f6c0abefce531d3eb227
Kei178 <keisuke.inaba.cpa@gmail.com>
2019-11-19 11:07:00 +1100

    Added Jest setup

---
+++ src/main.test.js
@@ -0,0 +1,9 @@
+import { shallowMount } from '@vue/test-utils'
+
+import App from './App'
+
+describe('App', () => {
+  it('renders correctly', () => {
+    const wrapper = shallowMount(App) 
+  })
+})
