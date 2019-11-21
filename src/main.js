be119f571ea73b441f69eeb60a6dac70aa19de84
Kei178 <keisuke.inaba.cpa@gmail.com>
2019-11-21 21:42:19 +1100

    Added semantic-ui

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store/store'
 
+// semantic-ui
+import SuiVue from 'semantic-ui-vue';
+import 'semantic-ui-css/semantic.min.css';
+
+Vue.use(SuiVue);
+
 new Vue({
   el: '#app',
   render: h => h(App),
