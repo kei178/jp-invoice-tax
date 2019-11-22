ad6dc29a6a34f48fd65da64768b8ac532484e8ac
Kei178 <keisuke.inaba.cpa@gmail.com>
2019-11-22 21:01:36 +1100

    Fixed build error vue-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path')
 var webpack = require('webpack')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
   entry: './src/main.js',
@@ -16,7 +17,8 @@ module.exports = {
           'vue-style-loader',
           'css-loader'
         ],
-      },      {
+      }, 
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
@@ -53,7 +55,10 @@ module.exports = {
   performance: {
     hints: false
   },
-  devtool: '#eval-source-map'
+  devtool: '#eval-source-map',
+  plugins: [
+    new VueLoaderPlugin()
+  ]
 }
 
 if (process.env.NODE_ENV === 'production') {
