d6aa245f0cb8513c84a46dda1b3a270fba943a35
Kei178 <keisuke.inaba.cpa@gmail.com>
2019-12-07 08:23:19 +1100

    Fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
   resolve: {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      '@': path.join(__dirname, './src')
     },
     extensions: ['*', '.js', '.vue', '.json']
   },
