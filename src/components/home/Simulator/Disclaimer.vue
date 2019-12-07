3ba9e6018dcad3b874ff46d38c79734961ffe38f
Kei178 <keisuke.inaba.cpa@gmail.com>
2019-12-07 17:38:01 +1100

    Updated for lambda test

--- src/components/home/Simulator/Disclaimer.vue
+++ src/components/home/Simulator/Disclaimer.vue
@@ -5,7 +5,7 @@
       <div class="item">税務上の判断は各人の責任でおこなってください。</div>
       <div class="item">青色申告の個人事業主（特にフリーランス）を前提にしています。</div>
       <div class="item">住民税（所得割）は「所得税の課税所得金額 x 10%」で計算しています。</div>
-      <div class="item">不動産所得は事業税課税標準の算出にのみ使っています。（インボイス制度の影響は無視しています）</div>
+      <div class="item">不動産所得は事業税課税標準の算出にのみ使っています（インボイス制度の影響は無視しています）。</div>
       <div class="item">実際の計算式は<a href="https://github.com/kei178/jp-invoice-tax" target="_blank">ソースコード</a>をご覧ください。</div>
     </div>
     <app-share></app-share>
