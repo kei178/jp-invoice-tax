23029be056e660b0df9e25d412fdfc88431e99da
Kei178 <keisuke.inaba.cpa@gmail.com>
2019-12-09 14:47:17 +1100

    Debug webpack env const

--- src/components/contact/Contact.vue
+++ src/components/contact/Contact.vue
@@ -147,8 +147,8 @@ export default {
     },
     submitForm() {
       if(confirm('こちらの内容で送信しますか？')) {
-        console('honeypot', this.honeypot)
-        console(process.env.VUE_APP_TELEGRAM_CHAT_ID)
+        console.log('honeypot', this.honeypot)
+        console.log(process.env.VUE_APP_TELEGRAM_CHAT_ID)
         if(this.isHoneypotEmpty()) this.notifyTelegram()
       }
     },
